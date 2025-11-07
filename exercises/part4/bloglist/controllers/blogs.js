9fc7388e158a2561e903200bc1b91c865b5fe220
lu-xingyu <estrellalxy@gmail.com>
2025-11-07 16:19:01 +0000

    part4 finished with tests and production tests

--- exercises/part4/bloglist/controllers/blogs.js
+++ exercises/part4/bloglist/controllers/blogs.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user')
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
--- exercises/part4/bloglist/models/user.js
+++ exercises/part4/bloglist/models/user.js
@@ -22,7 +22,8 @@ userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject.__v
-        delete returnedObject.password
+        delete returnedObject._id
+        delete returnedObject.passwordHash
     }
 })
 
