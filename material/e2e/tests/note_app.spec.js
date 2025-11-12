a9b3274a51c74161c59d59dfcffb642cb5c08e88
Esther Lu <estrellalxy@gmail.com>
2025-11-12 14:25:17 +0100

    part5 d material finished

--- material/e2e/tests/note_app.spec.js
+++ material/e2e/tests/note_app.spec.js
@@ -1,15 +1,79 @@
-const { test, describe, expect } = require('@playwright/test')
-
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { loginWith, createNote } = require('./helper')
 
 // page is an object provided by Playwright, representing a tab/page on broswer //('http://localhost:5173')
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('https://didactic-halibut-q7pxpr6qv64g346j7-5173.app.github.dev/')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
 
-    /*
+    await page.goto('/')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
-    */
+
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  })
+  
+  test('use can log in', async ({ page }) => {
+    await loginWith(page, 'mluukkai', 'salainen')
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await loginWith(page, 'mluukkai', 'wrong')
+
+    const errorDiv = page.locator('.error')
+    await expect(errorDiv).toContainText('wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+    await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await createNote(page, 'a note created by playwright')
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, 'a note created by playwright')
+      })
+  
+      test('importance can be changed', async ({ page }) => {
+        const note = page.locator('.note', {hasText: 'a note created by playwright'})
+        await note.getByRole('button', { name: 'make not important' }).click()
+        await expect(note.getByText('make important')).toBeVisible()
+      })
+    })
+
+    describe('and several notes exists', () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, 'first note')
+        await createNote(page, 'second note')
+        await createNote(page, 'third note')
+      })
+
+      test('one of those can be made nonimportant', async ({ page }) => {
+        const div = page.locator('li').filter({ hasText: 'third note' })
+        await div.getByRole('button').click()
+        
+        await expect(div.getByText('make important')).toBeVisible()
+      })
+    })
   })
 })
--- material/part4/app.js
+++ material/part4/app.js
@@ -1,6 +1,6 @@
+const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/note')
@@ -27,6 +27,11 @@ app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint) 
 app.use(middleware.errorHandler)
