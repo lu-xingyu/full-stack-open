2200738e3dbfc6dc12cc85d2e3f2ea0dab63bf8e
Esther-lxy <estrellalxy@gmail.com>
2025-11-20 10:53:14 +0000

    Thu, Nov 20, 2025, 11:53 AM +01:00

--- exercises/part7/routed-anecdotes/src/index.js
+++ exercises/part7/routed-anecdotes/src/index.js
@@ -3,5 +3,11 @@ import { useState } from 'react'
 const useField = (initial) => {
     const [value, setValue] = useState(initial)
 
-    const onChange = ()
+    const onChange = (newValue) => {
+        setValue(newValue)
+    }
+}
+
+export default {
+    value, onChange
 }
