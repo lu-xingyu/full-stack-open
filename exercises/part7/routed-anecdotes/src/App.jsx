9083af79ca330aadb9bd6b7dfbc49f9b7dcc90e5
Esther-lxy <estrellalxy@gmail.com>
2025-11-20 09:22:42 +0000

    Thu, Nov 20, 2025, 10:22 AM +01:00

--- exercises/part7/routed-anecdotes/src/App.jsx
+++ exercises/part7/routed-anecdotes/src/App.jsx
@@ -40,7 +40,7 @@ const Anecdote = ({ anecdotes }) => {
     <div>
       <h2>{anecdote.content}</h2>
       <p>has {anecdote.votes} votes</p>
-      <p>for more info see {anecdote.info}</p>
+      <p>for more info see <a href='#'>{anecdote.info}</a></p>
     </div>
   )
 }
