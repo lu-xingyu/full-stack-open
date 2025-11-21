c272dc566054b9d31117bb523352a27ab7c5cc96
Esther-lxy <estrellalxy@gmail.com>
2025-11-21 09:09:30 +0000

    Fri, Nov 21, 2025, 10:09 AM +01:00

--- material/part7/webpack/webpack.config.js
+++ material/part7/webpack/webpack.config.js
@@ -1,11 +1,13 @@
+// Webpack will automatically find webpack.config.js and execute the content of module.exports
+
 const path = require('path')
 
 const config = () => {
   return {
-    entry: './src/index.js',
+    entry: './src/index.js',   // entry of the whole application
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'main.js'
+      path: path.resolve(__dirname, 'build'),  // where to put the bundled  file
+      filename: 'main.js'  // the name of the bundled file
     }
   }
 }
