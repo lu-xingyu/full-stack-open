46724870068c2f97c4bf234bd8ce112e114d268c
Esther-lxy <estrellalxy@gmail.com>
2025-11-21 10:00:25 +0000

    Fri, Nov 21, 2025, 11:00 AM +01:00

--- material/part7/webpack/src/App.js
+++ material/part7/webpack/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react' // we need this now also in component files
+import React,  { useState }  from 'react' // we need this now also in component files
 import './index.css'
 
 const App = () => {
